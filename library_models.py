77aa68d3ed624c8077ca3da4f828081cf3b6ce0e
yushanshan <yushanshan@kuaishou.com>
2022-08-20 21:47:51 +0800

    update

--- library_models.py
+++ library_models.py
@@ -124,8 +124,8 @@ class JODIE(nn.Module):
     def multi_head_attention(self, neighbor_embeddings, target_embeddings, t_tensor, w_tensor, hidden_layer, weight_layer):
         hidden_neighbor = hidden_layer(neighbor_embeddings)
         hidden_target = hidden_layer(target_embeddings)
-        p = nn.Sigmoid()(self.attention_p(torch.cat([t_tensor, w_tensor], dim=1)))
-        c = nn.LeakyReLU()(weight_layer(torch.cat([target_embeddings.repeat(1,num_neighbors,1), neighbor_embeddings], dim=1) * p))
+        p = nn.Sigmoid()(self.attention_p(torch.cat([t_tensor, w_tensor], dim=2)))   # input batch_size,num_neighbor,2 / output batch_size,num_neighbor,1
+        c = nn.LeakyReLU()(weight_layer(torch.cat([target_embeddings.repeat(1,num_neighbors,1), neighbor_embeddings], dim=2) * p)) # batch_size,num_neighbor,2dim * batch_size,num_neighbor,1
         a = nn.Softmax()(torch.reshape(c, (-1, self.num_neighbor)))
         a_reshape = torch.reshape(a, (-1, self.num_neighbor, 1))
         out = torch.reshape(torch.sum(hidden_neighbor * a_reshape, dim=1), (-1, self.embedding_dim, 1)) # bacth_size, dim, 1
